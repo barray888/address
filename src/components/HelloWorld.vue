0a27eb3c200adf0ff12db031ec87579693f9b3f7
barray888 <1475460273@qq.com>
2019-02-22 18:05:17 +0800

    函数优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,9 @@
           <el-radio-group v-model="radio" @change="onRadioChange">
 
             <div class="text-sec" v-for="v in radioArr">
-              <el-radio :label="v.id">{{v.text}}</el-radio>
+              <el-radio :label="v.id">
+                <span v-html="v.text"> </span>
+              </el-radio>
             </div>
           </el-radio-group>
         </el-card>
@@ -84,7 +86,12 @@ export default {
         {
           id: 5,
           text: '河南省驻马店市新蔡县街道办事处 18087238726 丁某某 '
-        }
+        },
+        {
+          id: 6,
+          text: '四川成都市金牛区东风西路182号核工业商务楼602 \n' +
+                  ' 林某某 13888881234 \n'
+        },
       ],
       allProvince: ' 北京, 天津, 河北省, 山西省, 内蒙古自治区, 辽宁省, 吉林省, 黑龙江省, 上海, 江苏省, 浙江省, 安徽省, 福建省, 江西省, 山东省, 河南省, 湖北省, 湖南省, 广东省, 广西壮族自治区, 海南省, 重庆, 四川省, 贵州省, 云南省, 西藏自治区, 陕西省, 甘肃省, 青海省, 宁夏回族自治区, 新疆维吾尔自治区, 台湾, 香港特别行政区, 澳门特别行政区, 钓鱼岛',
       select: { province: '', city: '', area: '' },
@@ -115,58 +122,94 @@ export default {
       })
     },
     onTextInput(v){
+      let resObj = this.recAddressText(v)
+      this.form.username = resObj.name
+      this.select.province = resObj.province
+      this.select.city = resObj.city
+      this.select.area = resObj.area
+      this.form.detail = resObj.detail
+      this.form.phone = resObj.mobile
+    },
+    recAddressText(str){
       let that = this
-      let matched_str = (v + that.allProvince).replace(/(.+)(.+)\1/, '$1\n').split('\n')[0]
-      let correct_str = (v + that.allProvince).replace(/(.+)(.+)\1/, '$1\n').split('\n').join('').split(',')[0]
-      let is_find = that.allProvince.split(',').findIndex(
-              (value) => {
-                return value.replace(/\s+/g,"") === correct_str
-              }
-      )
-      if(is_find != -1){
-        v = v.replace(matched_str,correct_str)
+      // 去除左右空白
+      str = str.replace(/(^\s*)|(\s*$)/g, "");
+      let resObj = {
+        province: '',
+        city: '',
+        area: '',
+        detail: '',
+        name: '',
+        mobile: '',
       }
-      let reg = /.+?(省|市|自治区|自治州|县|区)/g;
-      let phone = v.match(/[0-9]\d{10,10}/g)
-      if(phone){
-        that.form.phone = phone[0]
-        v = v.replace(/[0-9]\d{10,10}/g,'')
-      }
-      let res = v.match(reg)
-      console.log(res)
-      if(res){
-        if( res.length > 0) {
-          res.forEach(function (e, i) {
-            if(i === 0){
-              let r = e.replace(/(^\s*)|(\s*$)/g, "").replace(/\s+/g," ").split(" ")
-              if(r.length === 1){
-                that.select.province = r[0]
-              }else if(r.length === 2){
-                r.forEach(function(res, k){
-                  if(that.allProvince.indexOf(res) != -1){
-                    that.select.province = res
-                  }else{
-                    that.form.username = res
-                  }
-                })
-              }
-              // that.select.province = e.replace(/\s+/g,"")
-            }
-            if(i === 1) that.select.city = e.replace(/\s+/g,"")
-            if(i === 2) {
-              that.select.area = e.replace(/\s+/g,"")
-              that.form.detail = v.substring(v.indexOf(e.replace(/\s+/g,"")) + e.length)
+      let tempArr = []; //["林某某", "13888881234", "四川成都市金牛区东风西路182号核工业商务楼602"] 不分顺序
+
+      // 如果有换行符 拆分并处理每个字符串的空白
+      if(str.indexOf('\n') != -1){
+        str.split('\n').forEach(function(v,k){
+          v.replace(/(^\s*)|(\s*$)/g, "").split(" ").forEach(function(v1,k1){
+            if(v1 != ''){
+              tempArr.push(v1)
             }
           })
-        }
+        })
       }else{
-        that.select.province = ''
-        that.select.city = ''
-        that.select.area = ''
-        that.form.detail = ''
-        that.form.phone = ''
-        that.form.username = ''
+        str.replace(/(^\s*)|(\s*$)/g, "").split(" ").forEach(function(v1,k1){
+          if(v1 != ''){
+            tempArr.push(v1)
+          }
+        })
       }
+
+      // console.log(tempArr)
+      tempArr.forEach(function(v,k){
+        if(v.length === 11 && Number(v) != 'NaN'){
+          resObj.mobile = v
+        }else if(v.length <= 5) {
+          resObj.name = v
+        }else {
+          // 处理地址
+          // 兼容没有省字的情况
+          let matched_str = (v + that.allProvince).replace(/(.+)(.+)\1/, '$1\n').split('\n')[0]
+          let correct_str = (v + that.allProvince).replace(/(.+)(.+)\1/, '$1\n').split('\n').join('').split(',')[0]
+          let is_find = that.allProvince.split(',').findIndex(
+                  (value) => {
+                    return value.replace(/\s+/g,"") === correct_str
+                  }
+          )
+          if(is_find != -1){
+            v = v.replace(matched_str,correct_str)
+          }
+          // console.log(v)
+          let reg = /.+?(省|市|自治区|自治州|县|区)/g;
+          let res = v.match(reg)
+
+          if(res){
+            if( res.length > 0) {
+              res.forEach(function (e, i) {
+                if(i === 0){
+                  let r = e.replace(/(^\s*)|(\s*$)/g, "").replace(/\s+/g," ").split(" ")
+                  if(r.length === 1){
+                    resObj.province = r[0]
+                  }else if(r.length === 2){
+                    r.forEach(function(res, k){
+                      if(that.allProvince.indexOf(res) != -1){
+                        resObj.province = res
+                      }
+                    })
+                  }
+                }
+                if(i === 1) resObj.city = e.replace(/\s+/g,"")
+                if(i === 2) {
+                  resObj.area = e.replace(/\s+/g,"")
+                  resObj.detail = v.substring(v.indexOf(e.replace(/\s+/g,"")) + e.length)
+                }
+              })
+            }
+          }
+        }
+      })
+      return resObj
     }
   }
 }
